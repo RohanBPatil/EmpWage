b2b8d2506bf7c395af82725119303852568daf91
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-26 15:25:21 +0530

    useCase1

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,13 @@ package EmpWage;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
+		
+		int is_full_time = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == is_full_time)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
